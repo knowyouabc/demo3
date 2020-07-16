ec18275a28fe1a28e6dcbbb89106ffb0bfad84f9
xujia <jxu17@best-inc.com>
2020-07-16 17:10:11 +0800

    xiugai

--- src/main/java/com/example/demo34/Demo34Application.java
+++ src/main/java/com/example/demo34/Demo34Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo34Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo34Application.class, args);
 	}
